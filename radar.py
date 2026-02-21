9ab11c51047ab7d7970727cdf8d0d9dff0651212
mercekmuhendislik-cloud <mercekmuhendislik@gmail.com>
2026-02-21 09:18:41 +0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -1,98 +1,26 @@
-import yfinance as yf
-import pandas as pd
-import numpy as np
-import warnings
-import requests
-import os
-from concurrent.futures import ThreadPoolExecutor
+name: BIST Radar Otomasyon
 
-warnings.filterwarnings('ignore')
+on:
+  schedule:
+    # Türkiye Saati (UTC+3) ile hafta içi her saat başı:
+    # 10:00, 11:00, 12:00, 13:00, 14:00, 15:00, 16:00, 17:00, 18:00
+    - cron: '0 7-15 * * 1-5'
+  workflow_dispatch:
 
-# --- 1. AYARLAR ---
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-def send_telegram_msg(message):
-    if not message.strip(): return
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": message, "parse_mode": "Markdown", "disable_web_page_preview": True}
-    try:
-        requests.post(url, json=payload)
-    except Exception as e:
-        print(f"Mesaj gönderilemedi: {e}")
-
-# --- 2. HESAPLAMA ---
-def calculate_t3(src, length, vf, multiplier):
-    def ema(s, l): return s.ewm(span=l, adjust=False).mean()
-    e1 = ema(src, length); e2 = ema(e1, length); e3 = ema(e2, length)
-    e4 = ema(e3, length); e5 = ema(e4, length); e6 = ema(e5, length)
-    vf3, vf2 = vf**3, vf**2
-    c1, c2, c3, c4 = -vf3, 3*vf2 + 3*vf3, -6*vf2 - 3*vf - 3*vf3, 1 + 3*vf + vf3 + 3*vf2
-    if multiplier != 3: c2, c3, c4 = 4*vf2 + 4*vf3, -8*vf2 - 4*vf - 4*vf3, 1 + 4*vf + vf3 + 4*vf2
-    return c1 * e6 + c2 * e5 + c3 * e4 + c4 * e3
-
-def check_formation(df_res, last_price):
-    if len(df_res) < 100: return False
-    src = (df_res['High'] + df_res['Low'] + 2 * df_res['Close']) / 4
-    t_siyah = calculate_t3(src, 50, 0.70, 3).iloc[-1]
-    t_mor   = calculate_t3(src, 87, 0.90, 4).iloc[-1]
-    t_sari  = calculate_t3(src, 37, 0.90, 4).iloc[-1]
-    return (t_sari < t_siyah < t_mor and last_price > t_sari)
-
-# --- 3. İŞLEME ---
-def process_ticker(ticker, data):
-    try:
-        df_h = data[ticker].dropna()
-        if len(df_h) < 200: return None
-        last_close = df_h['Close'].iloc[-1]
-        bb_mid = df_h['Close'].rolling(window=200).mean().iloc[-1]
-        
-        df_2s = df_h.resample('2h').agg({'High':'max', 'Low':'min', 'Close':'last'}).dropna()
-        df_4s = df_h.resample('4h').agg({'High':'max', 'Low':'min', 'Close':'last'}).dropna()
-
-        f1s, f2s, f4s = check_formation(df_h, last_close), check_formation(df_2s, last_close), check_formation(df_4s, last_close)
-        
-        skor = sum([f1s, f2s, f4s])
-        
-        # --- KRİTİK FİLTRE: Sadece 2 veya 3 sinyali olanları al ---
-        if skor >= 2:
-            t_name = ticker.replace(".IS","")
-            bb_emoji = "🟢" if last_close > bb_mid else "🔴"
-            link = f"[{t_name}](https://www.tradingview.com/chart/?symbol=BIST%3A{t_name})"
-            
-            # Sinyal detaylarını emoji ile daha şık yapalım
-            sinyaller = []
-            if f1s: sinyaller.append("1S")
-            if f2s: sinyaller.append("2S")
-            if f4s: sinyaller.append("4S")
-            
-            details = f"{link} | Fiyat: {last_close:.2f}\nBB Orta 200: {bb_emoji} | Sinyal: {'-'.join(sinyaller)}\n"
-            return {"skor": skor, "msg": details}
-    except: return None
-
-# --- 4. ANA DÖNGÜ ---
-if __name__ == "__main__":
-    # Hisse listesi (Hepsini tarar ama sadece kaliteli olanları mesaj atar)
-    bist_raw = "ACSEL, ADEL, ADESE, ADLVY, ADGYO, AFYON, AGHOL, AGESA, AGROT, AHSGY, AHGAZ, AKSFA, AKFK, AKMEN, AKCVR, AKBNK, AKCKM, AKCNS, AKDFA, AKYHO, AKENR, AKFGY, AKFIS, AKFYE, ATEKS, AKSGY, AKMGY, AKSA, AKSEN, AKGRT, AKSUE, AKTVK, ALCAR, ALGYO, ALARK, ALBRK, ALCTL, ALFAS, ALKIM, ALKA, AYCES, ALTNY, ALKLC, ALVES, ANSGR, AEFES, ANHYT, ASUZU, ANGEN, ANELE, ARCLK, ARDYZ, ARENA, ARFYE, ARMGD, ARSAN, ARSVY, ARTMS, ARZUM, ASGYO, ASELS, ASTOR, ATAGY, ATAVK, ATAKP, AGYO, ATLFA, ATSYH, ATLAS, ATATP, AVOD, AVGYO, AVTUR, AVHOL, AVPGY, AYDEM, AYEN, AYES, AYGAZ, AZTEK, BAGFS, BAHKM, BAKAB, BALAT, BALSU, BNTAS, BANVT, BARMA, BSRFK, BASGZ, BASCM, BEGYO, BTCIM, BSOKE, BYDNR, BAYRK, BERA, BRKT, BRKSN, BESLR, BJKAS, BEYAZ, BIENY, BIGTK, BLCYT, BLKOM, BIMAS, BINBN, BIOEN, BRKVY, BRKO, BIGEN, BRLSM, BRMEN, BIZIM, BLUME, BMSTL, BMSCH, BOBET, BORSK, BORLS, BRSAN, BRYAT, BFREN, BOSSA, BRISA, BULGS, BURCE, BURVA, BUCIM, BVSAN, BIGCH, CRFSA, CASA, CEMZY, CEOEM, CCOLA, CONSE, COSMO, CRDFA, CVKMD, CWENE, CGCAM, CAGFA, CMSAN, CANTE, CATES, CLEBI, CELHA, CLKMT, CEMAS, CEMTS, CMBTN, CMENT, CIMSA, CUSAN, DAGI, DAPGM, DARDL, DGATE, DCTTR, DGRVK, DMSAS, DENGE, DZGYO, DERIM, DERHL, DESA, DESPC, DEVA, DNISI, DIRIT, DITAS, DKVRL, DMRGD, DOCO, DOFER, DOHOL, DTRND, DGNMO, DOGVY, ARASE, DOGUB, DGGYO, DOAS, DOKTA, DURDO, DURKN, DUNYH, DNYVA, DYOBY, EBEBK, ECOGR, ECZYT, EDATA, EDIP, EFOR, EGEEN, EGGUB, EGPRO, EGSER, EPLAS, EGEGY, ECILC, EKER, EKIZ, EKOFA, EKOS, EKOVR, EKSUN, ELITE, EMKEL, EMNIS, EMIRV, EKGYO, EMVAR, ENJSA, ENERY, ENKAI, ENSRI, ERBOS, ERCB, EREGL, KIMMR, ERSU, ESCAR, ESCOM, ESEN, ETILR, EUKYO, EUYO, ETYAT, EUHOL, TEZOL, EUREN, EUPWR, EYGYO, FADE, FAIRF, FMIZP, FENER, FLAP, FONET, FROTO, FORMT, FRMPL, FORTE, FRIGO, FZLGY, GWIND, GSRAY, GARFA, GARFL, GRNYO, SNKRN, GEDIK, GEDZA, GLCVY, GENIL, GENTS, GEREL, GZNMI, GIPTA, GMTAS, GESAN, GLYHO, GOODY, GOKNR, GOLTS, GOZDE, GRTHO, GSDDE, GSDHO, GUBRF, GLRYH, GLRMK, GUNDG, GRSEL, SAHOL, HALKF, HLGYO, HLVKS, HRKET, HATEK, HATSN, HDFFL, HDFGS, HEDEF, HEKTS, HKTM, HTTBT, HOROZ, HUBVC, HUNER, HUZFA, HURGZ, ENTRA, ICBCT, ICUGS, INGRM, INVEO, INVES, ISKPL, IEYHO, IDGYO, IHEVA, IHLGM, IHGZT, IHAAS, IHLAS, IHYAY, IMASM, INALR, INDES, INFO, INTEK, INTEM, ISDMR, ISFAK, ISFIN, ISGYO, ISGSY, ISMEN, ISYAT, ISBIR, ISSEN, IZINV, IZENR, IZMDC, IZFAS, JANTS, KFEIN, KLKIM, KLSER, KAPLM, KRDMA, KRDMB, KRDMD, KAREL, KARSN, KRTEK, KARTN, KTLEV, KATMR, KAYSE, KENT, KRVGD, KERVN, KZBGY, KLGYO, KLRHO, KMPUR, KLMSN, KCAER, KCHOL, KOCMT, KLSYN, KNFRT, KONTR, KONYA, KONKA, KGYO, KORDS, KRPLS, KORTS, KOTON, KOPOL, KRGYO, KRSTL, KRONT, KSTUR, KUVVA, KUYAS, KBORU, KZGYO, KUTPO, KTSKR, LIDER, LIDFA, LILAK, LMKDC, LINK, LOGO, LKMNH, LRSHO, LUKSK, LYDHO, LYDYE, MACKO, MAKIM, MAKTK, MANAS, MAGEN, MARKA, MAALT, MRSHL, MRGYO, MARTI, MTRKS, MAVI, MZHLD, MEDTR, MEGMT, MEGAP, MEKAG, MNDRS, MEPET, MERCN, MERIT, MERKO, METRO, MTRYO, MEYSU, MHRGY, MIATK, MGROS, MSGYO, MPARK, MMCAS, MOBTL, MOGAN, MNDTR, MOPAS, EGEPO, NATEN, NTGAZ, NTHOL, NETAS, NIBAS, NUHCM, NUGYO, OBAMS, OBASE, ODAS, ODINE, OFSYM, ONCSM, ONRYT, ORCAY, ORGE, ORMA, OSMEN, OSTIM, OTKAR, OTTO, OYAKC, OYAYO, OYLUM, OZKGY, OZATD, OZGYO, OZRDN, OZSUB, OZYSR, PAMEL, PNLSN, PAGYO, PAPIL, PRFFK, PRDGS, PRKME, PARSN, PASEU, PSGYO, PAHOL, PATEK, PCILT, PGSUS, PEKGY, PENGD, PENTA, PSDTC, PETKM, PKENT, PETUN, PINSU, PNSUT, PKART, PLTUR, POLHO, POLTK, PRZMA, QFINF, QUAGR, RNPOL, RALYH, RAYSG, REEDR, RYGYO, RYSAS, RODRG, ROYAL, RGYAS, RTALB, RUBNS, SAFKR, SANEL, SNICA, SANFM, SANKO, SAMAT, SARKY, SARTN, SASA, SAYAS, SDTTR, SEGMN, SEKUR, SELEC, SELVA, SERNT, SRVGY, SEYKM, SILVR, SNGYO, SMRTG, SMART, SODSN, SOKE, SKTAS, SONME, SNPAM, SUMAS, SUNTK, SURGY, SUWEN, SEKFK, SEGYO, SKBNK, SOKM, TABGD, TNZTP, TARKM, TATGD, TATEN, TAVHL, TEKTU, TKFEN, TKNSA, TMPOL, TRHOL, TGSAS, TOASO, TRGYO, TRMET, TLMAN, TSPOR, TDGYO, TSGYO, TUCLK, TUKAS, TRCAS, TUREX, MARBL, TRILC, TCELL, TRKNT, TMSN, TUPRS, THYAO, PRKAB, TTKOM, TTRAK, TBORG, TURGG, GARAN, HALKB, ISCTR, TSKB, TURSG, SISE, VAKBN, UFUK, ULAS, ULUFA, ULUSE, ULUUN, USAK, ULKER, UNLU, VAKFN, VKGYO, VKFYO, VAKKO, VANGD, VBTYZ, VRGYO, VERUS, VERTU, VESBE, VESTL, VKING, YKBNK, YAPRK, YATAS, YYLGD, YAYLA, YGGYO, YEOTK, YGYO, YYAPI, YESIL, YBTAS, YIGIT, YONGA, YKSLN, YUNSA, ZGYO, ZEDUR, ZRGYO, ZOREN, BINHO"
-    stocks = [k.strip() + ".IS" for k in bist_raw.split(",") if k.strip()]
-    
-    print("Turbo tarama başladı...")
-    data = yf.download(stocks, period="60d", interval="1h", group_by='ticker', progress=False)
-
-    res_list = []
-    with ThreadPoolExecutor(max_workers=15) as exe:
-        futures = [exe.submit(process_ticker, t, data) for t in stocks]
-        for f in futures:
-            r = f.result()
-            if r: res_list.append(r)
-
-    if res_list:
-        full = [i['msg'] for i in res_list if i['skor'] == 3]
-        cift = [i['msg'] for i in res_list if i['skor'] == 2]
-
-        if full:
-            send_telegram_msg("🔥 *FULL KOMBO (KRİTİK)* 🔥\n\n" + "\n".join(full))
-        if cift:
-            send_telegram_msg("⭐ *ÇİFT SİNYAL (TAKİP)* ⭐\n\n" + "\n".join(cift))
-    else:
-        print("Kriterlere uyan elit sinyal bulunamadı.")
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Kodları Çek
+        uses: actions/checkout@v2
+      - name: Python Kur
+        uses: actions/setup-python@v2
+        with:
+          python-version: '3.9'
+      - name: Kütüphaneleri Yükle
+        run: pip install yfinance pandas numpy requests
+      - name: Radarı Çalıştır
+        env:
+          TELEGRAM_TOKEN: ${{ secrets.TELEGRAM_TOKEN }}
+          TELEGRAM_CHAT_ID: ${{ secrets.TELEGRAM_CHAT_ID }}
+        run: python radar.py
